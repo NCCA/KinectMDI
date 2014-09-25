d06d4abc7e972e5f15d7eb1f77cc653b0fb5c544
jmacey <jmacey@bournemouth.ac.uk>
2014-09-25 16:11:42 +0100

    initial commit

---
+++ include/ScreenQuad.h
@@ -0,0 +1,23 @@
+#ifndef SCREEN_QUAD_H__
+#define SCREEN_QUAD_H__
+#include <ngl/Vec2.h>
+#include <ngl/Vec3.h>
+class ScreenQuad
+{
+  public :
+  /// @brief ctor, note this will scale to any screen size so
+  /// the width and height are for the texture image passed in
+    ScreenQuad();
+    ~ScreenQuad();
+    void draw();
+
+
+  private :
+    GLuint m_vao;
+
+};
+
+
+
+
+#endif
